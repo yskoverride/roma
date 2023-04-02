76193c78aaff0e653e4fda99dd5b7b947427bd75
yskoverride <yskoverride@gmail.com>
2023-04-03 00:03:38 +0530

    Bug Fix for CreateFrameworkDirectoriesForTenant

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,10 +20,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-
-        TenantCreated::class => [
-            CreateFrameworkDirectoriesForTenant::class,
-        ],
     ];
 
     /**
