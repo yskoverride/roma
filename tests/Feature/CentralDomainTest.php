42050316d0a92e1b439623dac5e6ca09e8886829
yskoverride <yskoverride@gmail.com>
2023-04-29 15:01:40 +0530

    Removed unwanted test

--- tests/Feature/CentralDomainTest.php
+++ tests/Feature/CentralDomainTest.php
@@ -9,13 +9,5 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CentralDomainTest extends TestCase
 {
-    
-    use RefreshDatabase;
 
-    // public function test_central_domain_can_be_accessed(): void
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
 }
