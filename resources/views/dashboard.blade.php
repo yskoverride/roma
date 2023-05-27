147e3866659e3d802deaffa652cded28465f760a
yskoverride <yskoverride@gmail.com>
2023-05-27 15:30:49 +0530

    Changed Gender of Profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,11 @@
             <div class="min-w-0 flex-1">
               <!-- Profile -->
               <div class="flex items-center">
-                <img class="hidden h-16 w-16 rounded-full sm:block" src="https://xsgames.co/randomusers/avatar.php?g=female" alt="">
+                <img class="hidden h-16 w-16 rounded-full sm:block" src="https://xsgames.co/randomusers/avatar.php?g=male" alt="">
                 <div>
                   <div class="flex items-center">
                     <img class="h-16 w-16 rounded-full sm:hidden" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80" alt="">
-                    <h1 class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:leading-9">Good morning, {{ Auth::user()->name }}</h1>
+                    <h1 class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:leading-9">Hello, {{ Auth::user()->name }}</h1>
                   </div>
                   <dl class="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
                     <dt class="sr-only">Company</dt>
